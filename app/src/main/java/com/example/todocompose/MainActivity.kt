e905fff2b03a0aa6193146d2aa14662269500923
pack <phak080@gmail.com>
2021-12-03 13:30:18 +0700

    init project

---
+++ app/src/main/java/com/example/todocompose/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.todocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.todocompose.navigation.SetupNavigation
+import com.example.todocompose.ui.theme.TodoComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoComposeTheme {
+
+                navController = rememberNavController()
+                SetupNavigation(navHostController = navController)
+            }
+        }
+    }
+}
+
+/*
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TodoComposeTheme {
+        Greeting("Android")
+    }
+}*/
